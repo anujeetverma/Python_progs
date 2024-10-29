7c1d423ebfc8f725f92bfd724f936c5ceaf7d0b0
anujeetverma <167993759+anujeetverma@users.noreply.github.com>
2024-10-29 21:39:03 +0530

    git commit

---
+++ oops_Rev.py
@@ -0,0 +1,16 @@
+class Student():
+
+    def __init__(self,name, age, marks):
+        self.name = name
+        self.age = age
+
+    def get_avg(slef):
+        
+        
+
+    
+        
+        print("The name has been added")
+
+s1 = Student("Anujeet", 21, [23,234,23])
+print(s1.age)
